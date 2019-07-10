3ad06ac92bcd4d5306a90f53fd121b88ffd35874
volodymyr <wvlasenko@gmail.com>
2019-07-10 11:48:58 +0300

    first commit base by mustache

--- src/main/java/com/example/sweeter/GreetingsController.java
+++ src/main/java/com/example/sweeter/GreetingsController.java
@@ -1,17 +1,22 @@
 package com.example.sweeter;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import java.util.Map;
 
 @Controller
 public class GreetingsController {
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name = "name", required = false,
             defaultValue = "World")
-                                   String name, Model model) {
-        model.addAttribute("name", name);
+                                   String name, Map<String, Object> model) {
+        model.put("name", name);
         return "greeting";
+    }
+
+    @GetMapping
+    public String main(Map<String, Object> model) {
+        model.put("some", "Hello, lets code!");
+        return "main";
     }
 }
